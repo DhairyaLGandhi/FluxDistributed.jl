819432fb57a495198756c073834e96743cd41fd2
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-09-03 18:07:24 +0530

    add Documenter support

---
+++ docs/make.jl
@@ -0,0 +1,19 @@
+using Documenter
+using ResNetImageNet
+
+DocMeta.setdocmeta!(ResNetImageNet, :DocTestSetup, :(using ResNetImageNet); recursive = true)
+makedocs(modules = [ResNetImageNet],
+         doctest = VERSION == v"1.6",
+         sitename = "Data Parallel Training",
+         pages = ["Home" => "index.md",
+                  "Training" => "training.md",
+                  "Datasets" => "datasets.md"]
+         format = Documenter.HTML(
+             analytics = "UA-36890222-9",
+             assets = ["assets/flux.css"],
+             prettyurls = get(ENV, "CI", nothing) == "true"),
+         )
+
+deploydocs(repo = "github.com/DhairyaLGandhi/ResNetImageNet.jl.git",
+           target = "build",
+           push_preview = true)
