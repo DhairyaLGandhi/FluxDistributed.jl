86d124f95664d7c3e2d0cf995813eec27d49197b
Dhairya Gandhi <dhairya@juliacomputing.com>
2022-01-17 15:32:13 +0530

    Update driver.jl

--- bin/driver.jl
+++ bin/driver.jl
@@ -1,26 +1,26 @@
 using Distributed, Dates, DataFrames, DataSets, Optimisers
 
-addprocs(4, exeflags = "--project")
+addprocs(4, exeflags = ["--project", "-t8"])
 @everywhere include("main.jl")
 
-# key = ResNetImageNet.CSV.read("ext/final_keys.csv", DataFrame)
-key = open(BlobTree, DataSets.dataset("imagenet_cyclops")) do data_tree
+const key = open(BlobTree, DataSets.dataset("imagenet_cyclops")) do data_tree
   ResNetImageNet.train_solutions(data_tree, path"LOC_train_solution.csv", 1:1000)
 end
 
-l = ResNetImageNet.CSV.read("ext/final_labels.csv", DataFrame)
+# l = ResNetImageNet.CSV.read("ext/final_labels.csv", DataFrame)
 
-ls = open(BlobTree, DataSets.dataset("imagenet_cyclops")) do dt
+const ls = open(BlobTree, DataSets.dataset("imagenet_cyclops")) do dt
   ResNetImageNet.labels(dt)
 end
 
-classes = mapreduce(vcat, l[!,:label]) do x
-  findall(y -> y == x, ls[!,:label])
-end
+# classes = mapreduce(vcat, l[!,:label]) do x
+#   findall(y -> y == x, ls[!,:label])
+# end
 
-rcs = (RemoteChannel(() -> Channel(1), p) for p in workers()) |> collect
+const classes = 1:1000
 
-updated_grads_channel = (RemoteChannel(() -> Channel(1), 1) for p in workers()) |> collect
+const rcs = (RemoteChannel(() -> Channel(1), p) for p in workers()) |> collect
+const updated_grads_channel = (RemoteChannel(() -> Channel(1), 1) for p in workers()) |> collect
 
 function run_distributed(key, resnet, loss, rcs, updated_grads_channel; classes = 1:1000, kwargs...)
   rs = [resnet for _ in workers()];
