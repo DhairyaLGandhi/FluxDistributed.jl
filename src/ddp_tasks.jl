0347db390b5116c41e06a5bce7430023523faa81
Dhairya Gandhi <dhairya@juliacomputing.com>
2022-02-16 19:57:54 +0530

    dont using marking

--- src/ddp_tasks.jl
+++ src/ddp_tasks.jl
@@ -44,36 +44,38 @@ function destruct(o::T) where T
   end
 end
 
-function prepare_training(resnet, key, devices, nsamples; HOST = CUDA.CuDevice(0))
+function prepare_training(resnet, key, devices, opt, nsamples; HOST = CUDA.CuDevice(0))
   ds = Dict()
   ixs = map(shuffle!, collect.(collect(Iterators.partition(1:size(key, 1), size(key, 1) ÷ length(devices)))))[1:length(devices)]
   ks = map(x -> @view(key[x,:]), ixs)
-  # @sync for (dev, shard) in zip(devices, shards)
-  ns = ntuple(_ -> nsamples, 20) # length(devices))
+  ns = Iterators.repeated(nsamples, 25) # ntuple(_ -> nsamples, 5) # length(devices))
   buffer = Dict()
   zmodel = destruct(resnet)
+  st = Optimisers.init(opt, resnet)
   for dev in devices
     Threads.@spawn begin
       buffer[dev] = CUDA.device!(HOST) do
-        gpu(zmodel)
+        gpu(zmodel), gpu(st)
       end
     end
   end
+  dls = []
+  devs_and_ms = []
   for (k,dev) in zip(ks, devices)
-    Threads.@spawn begin
-      ds[dev] = CUDA.device!(dev) do
-        gpu(resnet),
-        Flux.Data.DataLoader((ns,), buffersize = 5) do x
-          shard = minibatch(nothing, k, nsamples = x, class_idx = 1:1000)
-          CUDA.device!(dev) do
-            gpu(shard)
-          end
+    CUDA.device!(dev) do
+      push!(devs_and_ms, (dev, gpu(resnet)))
+      dl = Flux.Data.DataLoader((ns,), buffersize = 5) do x
+        shard = minibatch(nothing, k, nsamples = x, class_idx = 1:1000)
+        CUDA.device!(dev) do
+          gpu(shard)
         end
       end
+      push!(dls, dl)
     end
   end
-  ds, buffer
+  (ds_and_ms = devs_and_ms, dls = dls), buffer
 end
+
 
 # function train(loss, dev, m, dl)
 #   CUDA.device!(dev) do
@@ -104,8 +106,6 @@ end
 #   @show "ended stuff"
 #   wait.(res)
 # end
-
-
 
 function train(loss, m, dl, opt, dev, (ip, op))
   # STEP 1: Take data from dataloader: this data is on `dev`
@@ -143,76 +143,61 @@ _copyto!(x, ::Nothing) = nothing
 _copyto!(::Nothing, x) = nothing
 
 function markbuffer!(dest, src, dev)
-  while true
-    if check(MARKER, dev, writable)
-      break
-    end
-  end
   Functors.fmap(src, dest) do x, y
     _copyto!(y, x)
     x
   end
-  mark!(MARKER, dev, readable)
-end
-
-function check(MARKER, dev, status)
-  MARKER[dev][] == status
-end
-
-"""
-    mark!(MARKER, dev)
-
-Mark that dev is changing state in MARKER
-
-MARKER is a Dict with keys as all the devices and the values being 2-tuples of atomic booleans which represent (hot gradient, can be overwritten)
-"""
-function mark!(MARKER, dev, status)
-  Threads.atomic_xchg!(MARKER[dev], status)  
 end
 
 function getbuffer!(dest, src, dev)
-  while true
-    if check(MARKER, dev, readable)
-      break
-    end
-  end
   Functors.fmap(dest, src) do x, y
     _copyto!(x, y)
     x
   end
-  mark!(MARKER, dev, writable)
 end
 
-const writable = true
-const readable = false
-const MARKER = Dict(dev => Threads.Atomic{Bool}(writable) for dev in CUDA.devices())
-
-function reset!(MARKER)
-  for k in keys(MARKER)
-    mark!(MARKER, k, writable)
+function sync_buffer(buffer)
+  vals = map(first, values(buffer))
+  # vals = collect(values(buffer))
+  final = reduce(vals[2:end], init = vals[1]) do x,y
+    Functors.fmap(x, y) do x, y
+       isnothing(x) && return y
+       isnothing(y) && return x
+       _accum(x,y)
+    end
   end
+end
+
+function step(buffer, dev, m, x, y)
+  gs, = CUDA.device!(dev) do
+    gradient(m -> ResNetImageNet.loss(m(x), y), m)
+  end
+  markbuffer!(first(buffer[dev]), gs, dev)
+  # markbuffer!(buffer[dev], gs, dev)
+  gs
 end
 
 # implementation of the `train` function from above
 # with actual semantics baked in
 # intended to be moved to the train method as pieces finish
 function train(setup, buffer, HOST = CUDA.CuDevice(0))
-  ts = []
-  reset!(MARKER)
-  for (dev,(m,dl)) in pairs(setup)
+  dls = nt.dls
+  ds_and_ms = nt.ds_and_ms
+  big_batches = zip(dls...)
+  for mbs in big_batches
     t = Threads.@spawn begin
-      for (x,y) in dl # STEP 1
-    
-        gm, = CUDA.device!(dev) do # STEP 2
-          gradient(m -> ResNetImageNet.loss(m(x), y), m)
-        end
-        markbuffer!(buffer[dev], gm, dev) # STEP 3
-        getbuffer!(gm, buffer[dev], dev) # STEP 4
-        m, st = opt(m, gm, st) # STEP 5
-        @info "results:" dev=dev nt=isa(gs, NamedTuple) s=sum(m(x)) t=(typeof(x), typeof(y)) host=HOST
+      t_ = map(ds_and_ms, mbs) do (dev, m), bs
+        @info "results:" dev=dev host=HOST
+        gs = Threads.@spawn step(buffer, dev, m, bs...)
       end
     end
-    push!(ts, t)
+    gs = fetch(t)
+    wait.(gs)
+    final = sync_buffer(buffer)
+    get_tasks = map(ds_and_ms, gs) do dnm, g
+     dev, m = dnm
+     get_task = Threads.@spawn getbuffer!(fetch(g), final, dev)
+    end
+    ts = fetch.(get_tasks)
   end
-  ts
 end
