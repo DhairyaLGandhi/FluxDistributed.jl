e3147780e3a4818119d08fe6e540c5be1b3b8db6
Dhairya Gandhi <dhairya@juliacomputing.com>
2022-02-20 03:20:17 +0530

    switch state api

--- src/overloads.jl
+++ src/overloads.jl
@@ -1,13 +1,26 @@
-function Optimisers.update(o, x::T, x̄, state) where T
+# function Optimisers.update(o, x::T, x̄, state) where T
+#   if x̄ === nothing
+#     return x, state
+#   elseif Functors.isleaf(x)
+#     return Optimisers._update(o, x, x̄, state)
+#   else
+#     x̄, _  = _functor(typeof(x), x̄)
+#     x, restructure = _functor(typeof(x), x)
+#     xstate = map((x, x̄, state) -> Optimisers.update(o, x, x̄, state), x, x̄, state)
+#     return restructure(map(first, xstate)), map(x -> x[2], xstate)
+#   end
+# end
+
+function Optimisers.update(o, state, x::T, x̄) where T
   if x̄ === nothing
-    return x, state
+    return state, x
   elseif Functors.isleaf(x)
-    return Optimisers._update(o, x, x̄, state)
+    return Optimisers._update(o, state, x, x̄)
   else
     x̄, _  = _functor(typeof(x), x̄)
     x, restructure = _functor(typeof(x), x)
-    xstate = map((x, x̄, state) -> Optimisers.update(o, x, x̄, state), x, x̄, state)
-    return restructure(map(first, xstate)), map(x -> x[2], xstate)
+    xstate = map((state, x, x̄) -> Optimisers.update(o, state, x, x̄), state, x, x̄)
+    return map(first, xstate), restructure(map(x -> x[2], xstate))
   end
 end
 
@@ -28,10 +41,19 @@ _functor(T, ref::Base.RefValue) = Functors.functor(T, ref.x)
 Optimisers.init(o, x) = nothing
 
 _accum(x, y) = Zygote.accum(x, y)
+_accum(x::T, y::T) where T <: Union{MaxPool, AdaptiveMeanPool, MeanPool, Flux.Zeros} = y
+_accum(x::Function, y::Function) = x
 _accum(x::Base.RefValue, y::Base.RefValue) = Ref(Zygote.accum(x.x, y.x))
 
 _dodiv(::Nothing, y) = nothing
 _dodiv(nt::NamedTuple, y) = NamedTuple{keys(nt)}((_dodiv(nt[k], y) for k in keys(nt)))
 _dodiv(x, y) = x / y
+_dodiv(x::Function, y) = x 
 _dodiv(x::Base.RefValue, y::Real) = Ref(_dodiv(x.x, y))
 _dodiv(x::AbstractArray, y::Real) = x / y
+_dodiv(x::T, y) where T <: Union{MaxPool, AdaptiveMeanPool, Flux.Zeros, MeanPool} = x
+
+_show_stats(::Nothing) = nothing
+_show_stats(nt::NamedTuple) = NamedTuple{keys(nt)}((_show_stats(nt[k]) for k in keys(nt)))
+_show_stats(x) = @show mean(x), sum(x), maximum(x), minimum(x)
+_show_stats(x::Base.RefValue) = Ref(_show_stats(x[]))
