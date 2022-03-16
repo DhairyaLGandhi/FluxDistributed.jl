f2e72a8f91d92ade4364d96a3a9715de12fb59e7
Dhairya Gandhi <dhairya@juliacomputing.com>
2022-03-17 02:25:15 +0530

    manually test the grads are added correctly

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,18 +3,16 @@ using CUDA
 using ResNetImageNet.Optimisers, ResNetImageNet.Functors
 using Test
 
-function compare(y::Tuple, ŷ)
+function compare(y::Tuple, ŷ::Tuple)
   foreach((a,b) -> compare(a, b), y, ŷ)
 end
 
-function compare(y::NamedTuple, ŷ)
+function compare(y::NamedTuple, ŷ::NamedTuple)
   foreach((a,b) -> compare(a, b), y, ŷ)
 end
 
-function compare(a::AbstractArray, b)
-  @testset "Arrays" begin
-    @test a ≈ b
-  end
+function compare(a::AbstractArray, b::AbstractArray)
+  @test a ≈ b
 end
 
 function compare(a::Base.RefValue, b::Base.RefValue)
@@ -22,15 +20,16 @@ function compare(a::Base.RefValue, b::Base.RefValue)
 end
 
 function compare(::Nothing, ::Nothing)
-  @testset "Nothings" begin
-    @test true
-  end
+  @test true
+end
+
+function compare(a::T, b::T) where T
+  fs = fieldnames(T)
+  foreach(f -> compare(getfield(a, f), getfield(b, f)), fs)
 end
 
 function compare(a, b)
-  @testset "Generic" begin
-    @test a ≈ b
-  end
+  @test a ≈ b
 end
 
 @testset "Single Device vs. Many Devices" begin
--- test/single_device.jl
+++ test/single_device.jl
@@ -1,19 +1,21 @@
-model = ResNet(34)
+model = ResNet(34);
 
 # First test the controlled case with a single GPU
 # and deterministic data
-data = rand(Float32, 224, 224, 3, 3)
+data = rand(Float32, 224, 224, 3, 3);
 
 gpu_model, gpu_data = CUDA.device!(CUDA.device()) do
   gpu(model), gpu(data)
 end
 
-gpu_grads = gradient(deepcopy(gpu_model)) do model
+gpu_model2 = deepcopy(gpu_model);
+
+gpu_grads = gradient(gpu_model) do model
   sum(model(gpu_data))
 end
 
 gpu_grads_x3 = map(1:3) do i
-  gradient(gpu_model) do model
+  gradient(deepcopy(gpu_model2)) do model
     sum(model(gpu_data[:,:,:,i:i]))
   end
 end
@@ -24,4 +26,11 @@ final = reduce(gpu_grads_x3[2:end], init = gpu_grads_x3[1]) do x, y
   end
 end
 
-compare(final, gpu_grads)
+# manually adding the first weight element from the first layer for every image independently
+function get_sum(gpu_grads_x3)
+  gpu_grads_x3[1][1].layers[1][1].layers[1].weight + gpu_grads_x3[2][1].layers[1][1].layers[1].weight + gpu_grads_x3[3][1].layers[1][1].layers[1].weight
+end
+
+@test "Check grads added correctly using `ResNetImageNet._accum`" get_sum(gpu_grads_x3) ≈ final[1].layers[1][1].layers[1].weight
+@test "Check manually added grads against batched grads" get_sum(gpu_grads_x3) ≈ gpu_grads[1].layers[1][1].layers[1].weight
+# compare(final, gpu_grads)
