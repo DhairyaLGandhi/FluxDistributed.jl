ce2174b8b3ff406f8bafc84783f7c73b4bb6c72d
Dhairya Gandhi <dhairya@juliacomputing.com>
2022-03-17 22:10:28 +0530

    add more tests - perhaps found the issue

--- test/single_device.jl
+++ test/single_device.jl
@@ -3,7 +3,7 @@ model = ResNet34();
 # First test the controlled case with a single GPU
 # and deterministic data
 
-function check_data_parallel(m, data = rand(Float32, 224,224,3,3))
+function check_data_parallel(m, data = rand(Float32, 224,224,3,3); field = :weight)
   gpu_model, gpu_data = gpu(m), gpu(data)
   gpu_model2 = deepcopy(gpu_model)
   
@@ -30,21 +30,24 @@ function check_data_parallel(m, data = rand(Float32, 224,224,3,3))
     end
 
     @testset "Manually accumulating grads against batched" begin
-      compare(get_sum(getfirst.(distributedgrads_x3, :weight)), getfirst(batchedgrads, :weight))
+      compare(get_sum(getfirst.(distributedgrads_x3, field)), getfirst(batchedgrads, field))
     end
   end
 end
 
-function get_sum(x)
-  # x[1][1].weight + x[2][1].weight + x[3][1].weight
-  +(x...)
-end
+get_sum(::AbstractVector{<:Nothing}) = nothing
+get_sum(x) = +(x...)
 
 # Check gradients are collected correctly for single layers
 @testset "Distributed Gradient accumulation" begin
   check_data_parallel(Conv((7,7), 3 => 4))
   check_data_parallel(Dense(10, 3), rand(Float32, 10, 3))
   check_data_parallel(Chain(Dense(10,5), Dense(5, 3)), rand(Float32, 10, 3))
+  check_data_parallel(MaxPool((3,3)), rand(Float32, 3,3,3,3))
+  for field in (:λ, :β, :γ)
+    check_data_parallel(BatchNorm(3), rand(Float32, 3,3,3,3), field = field)
+  end
+  check_data_parallel(Chain(Conv((7,7), 3 => 3), BatchNorm(3)), field = :weight)
 end
 
 # manually adding the first weight element from the first layer for every image independently
