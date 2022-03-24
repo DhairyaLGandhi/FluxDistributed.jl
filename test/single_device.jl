50c2721ce8782795e4cb3db43fd274a77adfcf66
Dhairya Gandhi <dhairya@juliacomputing.com>
2022-03-24 14:50:55 +0530

    retain comments about testing optimization

--- test/single_device.jl
+++ test/single_device.jl
@@ -167,7 +167,10 @@ end
   distributedgrad, batchedgrad = test_grad_syncing_in_train(loss, m, nt, buffer, opt, data, labels)
   compare(distributedgrad, batchedgrad)
 
-
+  # Every model gets data, produces grads
+  # These grads are synced - every model has identical grads now (verified in the single_device tests)
+  # the models are identical, the grads are identical, the states are identical (since they have only been initialized yet)
+  # therefore, the updated model, and states should be identical - test this
   @testset "Distributed Optimization" begin
     # Check distrbiuted optimization
     batchedmodel, batchedstate = opt(m, batchedgrad, st)
